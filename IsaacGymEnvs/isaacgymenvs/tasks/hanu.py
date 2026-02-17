68234faf02f01d01d229a1c96c374ffc48f83459
sincerem00n <jingjaijan62@gmail.com>
2026-02-18 00:25:45 +0700

    initial commit

---
+++ IsaacGymEnvs/isaacgymenvs/tasks/hanu.py
@@ -0,0 +1,43 @@
+import numpy as np
+import os
+import torch
+
+from isaacgym import gymtorch
+from isaacgym import gymapi
+from isaacgymenvs.utils.torch_jit_utils import scale, unscale, quat_mul, quat_conjugate, quat_from_angle_axis, \
+    to_torch, get_axis_params, torch_rand_float, tensor_clamp, compute_heading_and_up, compute_rot, normalize_angle
+
+from isaacgymenvs.tasks.base.vec_task import VecTask
+
+class Hanu(VecTask):
+
+    def __init__(self, cfg, rl_device, sim_device, graphics_device_id, headless, virtual_screen_capture, force_render):
+
+        self.cfg = cfg
+
+        self.max_episode_length = self.cfg["env"]["episode_length"]
+
+        self.randomization_params = self.cfg["task"]["randomization_params"]
+        self.randomize = self.cfg["task"]["randomize"]
+        self.dof_vel_scale = self.cfg["env"]["dofVelocityScale"]
+        self.contact_force_scale = self.cfg["env"]["contactForceScale"]
+        self.power_scale = self.cfg["env"]["powerScale"]
+        self.heading_weight = self.cfg["env"]["headingWeight"]
+        self.up_weight = self.cfg["env"]["upWeight"]
+        self.actions_cost_scale = self.cfg["env"]["actionsCost"]
+        self.energy_cost_scale = self.cfg["env"]["energyCost"]
+        self.joint_at_limit_cost_scale = self.cfg["env"]["jointAtLimitCost"]
+        self.death_cost = self.cfg["env"]["deathCost"]
+        self.termination_height = self.cfg["env"]["terminationHeight"]
+
+        self.debug_viz = self.cfg["env"]["enableDebugVis"]
+        self.plane_static_friction = self.cfg["env"]["plane"]["staticFriction"]
+        self.plane_dynamic_friction = self.cfg["env"]["plane"]["dynamicFriction"]
+        self.plane_restitution = self.cfg["env"]["plane"]["restitution"]
+
+        self.max_episode_length = self.cfg["env"]["episodeLength"]
+
+        self.cfg["env"]["numObservations"] = 108 #TODO: compute this from the robot DOF and other parameters
+        self.cfg["env"]["numActions"] = 30
+
+        super().__init__(cfg, rl_device, sim_device, graphics_device_id, headless, virtual_screen_capture, force_render)
